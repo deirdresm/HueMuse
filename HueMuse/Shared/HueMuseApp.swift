74471f4b6adc410028bb9b9bdf7a6c2dd1613a10
Deirdre Saoirse Moen <deirdre@deirdre.net>
2021-02-19 15:01:56 -0800

    Initial project commit.

---
+++ HueMuse/Shared/HueMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueMuseApp.swift
+//  Shared
+//
+//  Created by Deirdre Saoirse Moen on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct HueMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
